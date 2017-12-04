d6bb72a0a2e1d6c8e8a2f55af19038ce153f5490
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-12-04 21:53:46 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -7,7 +7,7 @@ def consolidate_cart(cart)
     item_array.each do |item,item_detail|
 
 binding.pry
-      if updated_hash[item]
+      if !updated_hash[item]
         updated_hash[item] = item_detail
         current_item= updated_hash[item]
         current_item[:count] = 1
